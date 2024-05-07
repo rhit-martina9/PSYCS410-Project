365b52653498414150cf410643c495f1148fdeff
rhit-loydma <loydma@rose-hulman.edu>
2024-05-07 15:52:10 -0400

    updates to functions

--- model_main.py
+++ model_main.py
@@ -1,7 +1,7 @@
-# Question would it be possible for us to enumerate our shapes? 
-# This way we could do somethin similar to our HW2 code, where we worked with a list of 5050 hypotheses 
-# Global array of shapes - each index has a list of points within the shape
-# Not sure how we would clearly indicate with points are at the corners or boundaries
+import numpy as np
+GRID_WIDTH = 11
+GRID_HEIGHT = 0
+
 
 def generate_rectangles():
     # returns a list of all possible rectangles - each rectangle is a list of points within the rectangle
@@ -26,13 +26,37 @@ def is_point_in_shape(shape, point):
 def generate_initial_distribution(shapes):
     # returns a 2D grid of probabilities for each hypothesis
     # this is based on just Bayes' rule and the size principle
-    pass
+    probs = np.zeros(len(shapes), GRID_WIDTH, GRID_HEIGHT)
+    for s in range(len(shapes)):
+        for i in range(0, GRID_WIDTH):
+            for j in range(0, GRID_HEIGHT):
+                pass
+                # if shapes[s].isPointInShape((i,j)):
+                    # probs[s][i][j] = 1 / shapes[s].numPointsInShape()
+    return probs
+
+def generate_prior(shapes):
+    # returns the prior probability for a list of shapes
+    prior = np.zeros(len(shapes))
+    for s in range(len(shapes)):
+        prior[s] = 1 / len(shapes)
+    return prior
 
 def generate_predictions(shapes, alpha, num_interations):
     # get init distributions and then apply recursive process
     # returns a 2D grid of probabilities for each hypothesis
-    pass
-
+    probs = generate_initial_distribution()
+    prior = generate_prior(shapes)
+    new_probs = np.zeros(len(shapes), GRID_WIDTH, GRID_HEIGHT)
+    for k in range(num_interations):
+        for s in range(len(shapes)):
+            for i in range(0, GRID_WIDTH):
+                for j in range(0, GRID_HEIGHT):
+                    new_probs[s][i][j] = (probs[s][i][j] * prior[s]) ^ alpha
+        for s in range(len(shapes)):
+            new_probs[s] /= np.sum(new_probs[s])
+        probs = new_probs
+    return probs
 
 def main():
     #gen all shapes
